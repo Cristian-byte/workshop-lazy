2641e42b924b3dfdfe91faae333dd4d0a4dd1bd0
Cristian Sanchez Flores <andygign40@gmail.com>
2022-02-04 17:57:18 -0600

    5.3 Creando imagenes con JavaScript

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//Crear imagen  
+//Agregar a #imagen
+
+const maximum = 123;
+const minimum = 1;
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+
+const createImageNode = () => {
+    const container = document.createElement("div")
+    container.className = "p-4";
+
+    const imagen = document.createElement("img")
+    imagen.className = "mx-auto";
+    imagen.width = "300";
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen);
+
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+
+const addButton = document.querySelector("button")
+
+//
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.appendChild(nuevaImagen);
+};
+
+addButton.addEventListener("click", addImage);
